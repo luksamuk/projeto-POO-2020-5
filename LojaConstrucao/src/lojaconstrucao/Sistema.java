e6c8d10c8eb862b381d91d6971e8a902539a648f
Lucas S. Vieira <lucasvieira@protonmail.com>
2020-12-09 22:24:51 -0300

    Cria classes necessárias para o projeto

---
+++ LojaConstrucao/src/lojaconstrucao/Sistema.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lojaconstrucao;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ *
+ * @author alchemist
+ */
+public class Sistema {
+    
+    public Sistema() {
+        
+    }
+    
+}
